e61d0dfc6696f58c1ec1ba5aa94a27c412c2d94a
clorin <clorin@student.42.fr>
2023-03-29 18:25:07 +0200

    in progress

--- tutos/test.py
+++ tutos/test.py
@@ -8,8 +8,9 @@ files_name = mne.datasets.eegbci.load_data(subject=1, runs=[3, 7, 11],path=os.ge
 print(files_name)
 
 raw = mne.io.concatenate_raws([mne.io.read_raw_edf(f, preload=True) for f in files_name])
+print(raw.info)
 sfreq = raw.info['sfreq']
-print(f"sfreq = {sfreq}")
+raw.plot(scalings=dict(eeg=250e-6), title='Before traitement')
 
 events, event_id = mne.events_from_annotations(raw, event_id=dict(T0=0, T1=1, T2=2), verbose=True)
 print(events.shape)
@@ -20,6 +21,7 @@ print(annotations)
 
 fig = mne.viz.plot_events(events, sfreq=sfreq, first_samp=raw.first_samp, event_id=event_id)
 fig.subplots_adjust(right= 0.7)
+fig.title('Events')
 plt.show()
 
 raw = raw.set_annotations(annotations=annotations, verbose = True)
@@ -37,4 +39,37 @@ plt.show()
 raw.compute_psd(picks='all').plot()
 raw.compute_psd(picks='all').plot(average=True)
 
+plt.show()
+
+#ICA
+ica = mne.preprocessing.ICA(n_components=62, random_state=0)
+dir(ica)
+raw_copy = raw.copy().filter(8,30)
+raw_copy.drop_channels(['T9', 'T10'])
+ica.fit(raw_copy)
+print(raw.ch_names)
+ica.plot_components(outlines='head')
+
+# Select channels
+channels = raw.info["ch_names"]
+good_channels = ["FC5", "FC3", "FC1", "FCz", "FC2", "FC4", "FC6",
+                    "C5",  "C3",  "C1",  "Cz",  "C2",  "C4",  "C6",
+                    "CP5", "CP3", "CP1", "CPz", "CP2", "CP4", "CP6"]
+bad_channels = [x for x in channels if x not in good_channels]
+
+print(bad_channels)
+
+raw.drop_channels(bad_channels)
+raw.compute_psd().plot_topomap()
+
+# Apply band-passfilter
+raw.notch_filter(60, method="iir")
+raw.compute_psd().plot()
+plt.show()
+
+raw.filter(7.0, 32.0, fir_design="firwin")
+raw.compute_psd().plot()
+plt.show()
+
+raw.plot(scalings=dict(eeg=250e-6))
 plt.show()
